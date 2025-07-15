26584b0de3570dab81fa8da6e5ef8154f85a376e
Nishchay Makkar <nishchaymakkar5@gmail.com>
2025-07-15 17:56:49 +0530

    feat: Integrate IntroPage and google_nav_bar
    This commit introduces the `IntroPage` as the initial screen of the application.
    
    It also adds the `google_nav_bar` package as a dependency to facilitate navigation.
    
    Key changes:
    - Set `IntroPage` as the `home` widget in `MaterialApp`.
    - Added `google_nav_bar: ^5.0.6` to `pubspec.yaml`.
    - Included assets from `lib/sneakers/`.
    - Updated `pubspec.lock` with the new dependency.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/pages/intro_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntroPage(),
+    );
   }
 }
